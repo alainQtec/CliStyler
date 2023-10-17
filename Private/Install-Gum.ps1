42ebb9096ec12cca194974e6fd63cfb945a34c28
Alain Herve <alainQtec@gmail.com>
2023-10-18 00:27:26 +0200

    Create Install-Gum.ps1

---
+++ Private/Install-Gum.ps1
@@ -0,0 +1,31 @@
+function Install-Gum {
+    <#
+    .SYNOPSIS
+        A short one-line action-based description, e.g. 'Tests if a function is valid'
+    .DESCRIPTION
+        A longer description of the function, its purpose, common use cases, etc.
+    .NOTES
+        Information or caveats about the function e.g. 'This function is not supported in Linux'
+    .LINK
+        https://github.com/charmbracelet/gum/releases/download/v0.11.0/gum_0.11.0_Windows_x86_64.zip
+    .EXAMPLE
+        Test-MyTestFunction -Verbose
+        Explanation of the function or its result. You can include multiple examples with additional .EXAMPLE lines
+    #>
+    [CmdletBinding()]
+    param (
+
+    )
+
+    begin {
+
+    }
+
+    process {
+
+    }
+
+    end {
+
+    }
+}
