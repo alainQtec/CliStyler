69a80ab431a55e51853facc9f792803698d908a3
Alain Herve <alainQtec@gmail.com>
2023-10-18 00:27:50 +0200

    Create Install-VSH.ps1

---
+++ Private/Install-VSH.ps1
@@ -0,0 +1,32 @@
+﻿function Install-VSH {
+    <#
+    .SYNOPSIS
+        A short one-line action-based description, e.g. 'Tests if a function is valid'
+    .DESCRIPTION
+        A longer description of the function, its purpose, common use cases, etc.
+    .NOTES
+        Information or caveats about the function e.g. 'This function is not supported in Linux'
+    .LINK
+        https://github.com/charmbracelet/vhs
+        https://github.com/charmbracelet/vhs/releases/download/v0.6.0/vhs_0.6.0_Windows_x86_64.zip
+    .EXAMPLE
+        Test-MyTestFunction -Verbose
+        Explanation of the function or its result. You can include multiple examples with additional .EXAMPLE lines
+    #>
+    [CmdletBinding()]
+    param (
+
+    )
+
+    begin {
+
+    }
+
+    process {
+
+    }
+
+    end {
+
+    }
+}
