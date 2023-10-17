8ee912c4774d7eeb4d6f519c16a34c47e9cb681c
Alain Herve <alainQtec@gmail.com>
2023-10-18 00:27:06 +0200

    Create Install-Glow.ps1

---
+++ Private/Install-Glow.ps1
@@ -0,0 +1,31 @@
+﻿function Install-Glow {
+    <#
+    .SYNOPSIS
+        A short one-line action-based description, e.g. 'Tests if a function is valid'
+    .DESCRIPTION
+        A longer description of the function, its purpose, common use cases, etc.
+    .NOTES
+        Information or caveats about the function e.g. 'This function is not supported in Linux'
+    .LINK
+        https://github.com/charmbracelet/glow/releases/download/v1.5.1/glow_Windows_x86_64.zip
+    .EXAMPLE
+        Test-MyTestFunction -Verbose
+        Explanation of the function or its result. You can include multiple examples with additional .EXAMPLE lines
+    #>
+    [CmdletBinding()]
+    param (
+
+    )
+
+    begin {
+
+    }
+
+    process {
+
+    }
+
+    end {
+
+    }
+}
