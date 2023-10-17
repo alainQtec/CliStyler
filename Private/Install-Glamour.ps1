2bd3a21810538f89987bb0b668b7faf66d6ee327
Alain Herve <alainQtec@gmail.com>
2023-10-18 00:26:43 +0200

    Create Install-Glamour.ps1

---
+++ Private/Install-Glamour.ps1
@@ -0,0 +1,31 @@
+﻿function Install-Glamour {
+    <#
+    .SYNOPSIS
+        glamour
+    .DESCRIPTION
+        A longer description of the function, its purpose, common use cases, etc.
+    .NOTES
+        Information or caveats about the function e.g. 'This function is not supported in Linux'
+    .LINK
+        glamour
+    .EXAMPLE
+        Test-MyTestFunction -Verbose
+        Explanation of the function or its result. You can include multiple examples with additional .EXAMPLE lines
+    #>
+    [CmdletBinding()]
+    param (
+
+    )
+
+    begin {
+
+    }
+
+    process {
+
+    }
+
+    end {
+
+    }
+}
